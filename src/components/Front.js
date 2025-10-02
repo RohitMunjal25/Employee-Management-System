bdebf8cd37d55c634d5f40d4f1ab7a59bee4f086
RohitMunjal25 <mobilemunjal@gmail.com>
2025-10-03 01:37:26 +0530

    final commit

--- src/components/Front.js
+++ src/components/Front.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './styling/Front.css'; 
-import api from "./Apicall";
+
 export default function Front() {
   
   return (
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom"; 
 import './styling/Menu.css';
-import api from "./Apicall";
+
 
 export default function Menu() {
   
